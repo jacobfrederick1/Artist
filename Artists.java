4e2a26d8e84a1272eb8c5764861887370f483ebd
Jacob Frederick <frederickjacob1@outlook.com>
2020-12-04 14:56:55 -0600

    add methods to add,remove, update and display a hashMap of artists whose values are an Artist object

--- Artists.java
+++ Artists.java
@@ -7,31 +7,109 @@ import java .io.*;
  */
 public class Artists{
     
-    Map<String,Artist> performers;
+    private Map<String,Artist> artists;
+    String result = "";
 
     public Artists(){
-        performers = new HashMap<>();
+        artists = new HashMap<>();
     }
 
-    public void process(String file){
-        
-        try {
-            File infile = new File(file);
+    //reads in artist from a file and creates an Artist object for each non duplicate
+    public String process(String dataFile){
+        try{
+            File infile = new File(dataFile);
             Scanner input = new Scanner(infile);
-
-            // Test if file has text
-            while (input.hasNext()) {
-                String key = input.next();
-                if(performers.containsKey(key)){
-                    performers.put(input.next(), new Artist(input.next(),input.next()));
+            String statements = "";
+            while(input.hasNext()){
+                String artist = input.next();
+                if(!exist(artist)){
+                    artists.put(artist,new Artist(input.next(),input.next(),input.next(),input.next()));
+                    statements += artist + " was successfully added\n";
+                }
+                else{
+                    statements += artist + "was already added\n";
                 }
             }
             input.close();
+            return statements;
         }
-        // catches exception and prints error statement
-        catch (Exception e) {
-            System.out.print("The file was not found!");
+        catch(FileNotFoundException exception){
+            return "Error file not found";
         }
     }
+
+    //adds an artist to artists with a default Artist constructor.
+    public String addArtist(String name){
+        if(!exist(name)){
+            artists.put(name,new Artist());
+            result = name + "was successfully added";
+        }
+        else{
+            result = name + "was already added";
+        }
+        return result;
+    }
     
+    //updates all values for an artist
+    public String changeValue(String name,String genre,String label,String location,String date){
+        if(exist(name)){
+            artists.replace(name, artists.get(name), new Artist(genre,label,location,date));
+            result = name + "'s values have been updates";
+        }
+        else{
+            result = name + "could not be changed because it does not exist";
+        }
+        return result;
+    }
+
+    //retuns the artist object
+    public String getArtist(String name){
+        if(exist(name)){
+            artists.get(name);
+        }
+        else{
+            result = name + "is not accessable";
+        }
+        return result;
+    }
+
+    //returns a map of the artist
+    public Map<String, Artist> getArtists() {
+        return artists;
+    }
+
+    //returns a set of all the artist
+    public Set<String> getArtistKeysSet(){
+        return artists.keySet();
+    }
+
+    //returns all of the values in artists
+    public Collection getArtistValues(){
+        return artists.values();
+    }
+
+    //removes an artist and their values
+    public String removeArtist(String name){
+        if(exist(name)){
+            artists.remove(name);
+            result = name + "was successfully removed";
+        }
+        else{
+            result = name + "could not be removed because it does not exist";
+        }
+        return result;
+    }
+
+    //test if an artist exist
+    public boolean exist(String name){
+        if(artists.containsKey(name))
+            return true;
+        else
+            return false;
+    }
+
+    public String toString() {
+        return "" + artists;
+    }
+
 }
